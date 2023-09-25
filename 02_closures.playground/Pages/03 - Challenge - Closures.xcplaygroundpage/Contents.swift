8106bade161e7f57e447730018beddd157dbcc81
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-25 15:51:54 +0200

    challenge closures

--- 02_closures.playground/Pages/03 - Challenge - Closures.xcplaygroundpage/Contents.swift
+++ 02_closures.playground/Pages/03 - Challenge - Closures.xcplaygroundpage/Contents.swift
@@ -5,35 +5,43 @@
  ## Challenge 1
  - Create a closure version of the function below.
  - Try out the function & closure!
-*/
+ */
 
 // --------------------------------------
 func calculateFullName(firstName: String, lastName: String?) -> String {
-  firstName + " " + (lastName ?? "")
+    firstName + " " + (lastName ?? "")
 }
+
 // --------------------------------------
 
 // TODO: Write solution here
 
+let calculateFullNameClosure = { (firstName: String, lastName: String?) -> String in
+    firstName + " " + (lastName ?? "")
+}
+
+calculateFullNameClosure("Jan", "Armbrust")
+calculateFullName(firstName: "Jan", lastName: "Armbrust")
 
 /*:
  ## Challenge 2
  - Call the `printResult` function below
  - Use an inline closure as an argument
-*/
+ */
 
 // --------------------------------------
 typealias Operate = (Double, Double) -> Double
 
 func printResult(_ operate: Operate, _ a: Double, _ b: Double) {
-  let result = operate(a, b)
-  print(result)
+    let result = operate(a, b)
+    print(result)
 }
+
 // --------------------------------------
 
 // TODO: Write solution here
 
-
-
-
+printResult({ (a: Double, b: Double) -> Double in
+    (a * a) + (b * b)
+}, 2, 5)
 //: [⇒ Next: 04 - Closure Syntax](@next)
