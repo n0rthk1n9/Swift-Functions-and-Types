b422b405204046c40bcb6b9495d3c9b059bb83ed
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-25 14:56:15 +0200

    learn functions as parameters

--- 01_functions.playground/Pages/07 - Functions as Parameters.xcplaygroundpage/Contents.swift
+++ 01_functions.playground/Pages/07 - Functions as Parameters.xcplaygroundpage/Contents.swift
@@ -2,17 +2,33 @@
 //: ## Episode 07 - Functions as Parameters
 
 //: Functions are data types, and can be assigned to variables and constants just like `Int` and `String`.
+func add(number1: Int, number2: Int) -> Int {
+    number1 + number2
+}
 
+var function = add
+function(4, 2)
 
+func subtract(number1: Int, number2: Int) -> Int {
+    number1 - number2
+}
 
+function = subtract
+function(4, 2)
 
+typealias Operate = (Int, Int) -> Int
 
-//: Functions can use functions as parameter and return types. Functions that do this are called  "higher-order functions"
+func printResult(_ operate: Operate, _ a: Int, _ b: Int) {
+    let result = operate(a, b)
+    print(result)
+}
 
-
-
-
-
+printResult(add, 4, 2)
+printResult(subtract, 4, 2)
+printResult(+, 4, 2)
+printResult(-, 4, 2)
+printResult(*, 4, 2)
+printResult(/, 4, 2)
 
 //: A `typealias` lets you give a name to a compound type, or give *another* name to a named type.
 
@@ -24,7 +40,7 @@ let jessy: Student = (name: "Jessy", grade: 49)
 let ozma: Student = (name: "Ozma", grade: 87)
 
 func printPassStatus(for student: Student) {
-  print(student.grade >= passingGrade ? "\(student.name) passed!" : "Try again, \(student.name).")
+    print(student.grade >= passingGrade ? "\(student.name) passed!" : "Try again, \(student.name).")
 }
 
 printPassStatus(for: jessy)
