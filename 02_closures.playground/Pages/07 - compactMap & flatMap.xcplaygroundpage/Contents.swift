c74f91b0eaca5bd9d009ff3dd85bec1dd48a036f
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-26 18:03:08 +0200

    learn compactMap and flatMap

--- 02_closures.playground/Pages/07 - compactMap & flatMap.xcplaygroundpage/Contents.swift
+++ 02_closures.playground/Pages/07 - compactMap & flatMap.xcplaygroundpage/Contents.swift
@@ -7,20 +7,39 @@
 let userInput = ["meow!", "15", "37.5", "seven"]
 // --------------------------------------
 
+var arrayForValidInput: [Int] = []
+for input in userInput {
+    guard let input = Int(input) else {
+        continue
+    }
+    arrayForValidInput.append(input)
+}
 
+print(arrayForValidInput)
 
+let validInput = userInput.compactMap { input in
+    Int(input)
+}
 
-
+print(validInput)
 
 //: `flatMap`
 
 // --------------------------------------
 let arrayOfDwarfArrays = [
-  ["Sleepy", "Grumpy", "Doc", "Bashful", "Sneezy"],
-  ["Thorin", "Nori", "Bombur"]
+    ["Sleepy", "Grumpy", "Doc", "Bashful", "Sneezy"],
+    ["Thorin", "Nori", "Bombur"]
 ]
 // --------------------------------------
 
+let dwarvesAfterM = arrayOfDwarfArrays.flatMap { dwarves -> [String] in
+    var afterM: [String] = []
+    for dwarf in dwarves where dwarf > "M" {
+        afterM.append(dwarf)
+    }
+    return afterM
+}
 
+print(dwarvesAfterM)
 
 //: [⇒ Next: 08 - Challenge: Closures & Collections](@next)
