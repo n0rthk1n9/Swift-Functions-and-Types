b5dce2c031a0fd06f3e2d32cb235a28453141804
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-26 17:44:30 +0200

    challenge closures

--- 02_closures.playground/Pages/05 - Challenge - Closure Syntax.xcplaygroundpage/Contents.swift
+++ 02_closures.playground/Pages/05 - Challenge - Closure Syntax.xcplaygroundpage/Contents.swift
@@ -8,18 +8,22 @@
   * No parameter or return types
   * No parameter names
  Try passing the same values into each closure. Their printed results should match!
-*/
+ */
 
 // --------------------------------------
-let copyLines = { (offense: String, repeatCount: Int) -> Void in
-  print( String(repeating: "I must not \(offense).", count: repeatCount) )
+let copyLines = { (offense: String, repeatCount: Int) in
+    print(String(repeating: "I must not \(offense).", count: repeatCount))
 }
+
 // --------------------------------------
 
 // TODO: Write solution here
+typealias OffenseRepetition = (String, Int) -> Void
 
+let copyLinesNoParameterTypesAndReturnTypes: OffenseRepetition = { offense, repeatCount in
+    print(String(repeating: "I must not \(String(offense)).", count: repeatCount))
+}
 
-
-
-
+copyLines("lie", 18)
+copyLinesNoParameterTypesAndReturnTypes("lie", 18)
 //: [⇒ Next: 06 - forEach and map](@next)
