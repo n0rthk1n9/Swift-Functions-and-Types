6585fb2798f567454f20fdbffd9720001fce4aad
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-28 19:20:58 +0200

    challenge filter, reduce and sort

--- 02_closures.playground/Pages/10 - Challenge - filter, reduce, & sort.xcplaygroundpage/Contents.swift
+++ 02_closures.playground/Pages/10 - Challenge - filter, reduce, & sort.xcplaygroundpage/Contents.swift
@@ -3,34 +3,23 @@
 
 /*:
  ## Challenge Time 😎
- 
+
  Create a constant array called `names` which contains some names as strings (any names will do — make sure there’s more than three though!). Now use `reduce` to create a string which is the concatenation of each name in the array.
-*/
-
-
-
-
+ */
+let names = ["Jan", "Ossi", "Flecki", "Steve", "Tim"]
+print(names.reduce("") { $0 + $1 })
 
 /*:
  Using the same `names` array, first filter the array to contain only names which have more than four characters in them, and then create the same concatenation of names as in the above exercise. (Hint: you can chain these operations together.)
-*/
-
-
-
-
-
+ */
+names.filter { $0.count > 3 }.reduce("") { $0 + $1 }
 /*:
  Create a constant dictionary called `namesAndAges` which contains some names as strings mapped to ages as integers. Now use `filter` to create a dictionary containing only people under the age of 18.
-*/
-
-
-
-
-
+ */
+let namesAndAges = ["Jan": 31, "Ossi": 2, "Flecki": 4, "Steve": 56, "Tim": 62]
+let peopleUnder18 = namesAndAges.filter { $0.value < 18 }
 /*:
  Using the same `namesAndAges` dictionary, filter out the adults (those 18 or older) and then use `map` to convert to an array containing just the names (i.e. drop the ages).
-*/
-
-
-
+ */
+let peopleNamesOver18 = namesAndAges.filter { $0.value > 18 }.map { $0.key }
 //: [⇒ Next: 11 - Conclusion](@next)
