57022d79bef35842beec2d9f7e18adaf0d7fc9e8
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-29 16:17:59 +0200

    learn switch statements

--- 02_enumerations.playground/Pages/04 - Switch Statements.xcplaygroundpage/Contents.swift
+++ 02_enumerations.playground/Pages/04 - Switch Statements.xcplaygroundpage/Contents.swift
@@ -2,20 +2,31 @@
 //: ## Episode 04: Switch Statements
 
 enum Month: Int {
-  case january = 1, february, march, april, may, june, july, august, september, october, november, december
+    case january = 1, february, march, april, may, june, july, august, september, october, november, december
 }
 
 enum Season: String, CaseIterable {
-  /// ☃️
-  case winter
-  /// 🌸
-  case spring
-  /// 😎
-  case summer
-  /// 🍂
-  case autumn
+    /// ☃️
+    case winter
+    /// 🌸
+    case spring
+    /// 😎
+    case summer
+    /// 🍂
+    case autumn
 }
 
-
+func getSeason(for month: Month) -> Season {
+    switch month {
+    case .december, .january, .february:
+        return .winter
+    case .march, .april, .may:
+        return .spring
+    case .june, .july, .august:
+        return .summer
+    case .september, .october, .november:
+        return .autumn
+    }
+}
 
 //: [⇒ Next: 05 - More Switch Statements](@next)
