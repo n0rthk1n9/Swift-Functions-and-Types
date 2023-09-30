58eb4e190754c4c6b113ba9b19ef7dd0c9d9d142
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-30 16:20:04 +0200

    learn more inheritance

--- 05_protocols_and_inheritance.playground/Pages/02 - Inheritance.xcplaygroundpage/Contents.swift
+++ 05_protocols_and_inheritance.playground/Pages/02 - Inheritance.xcplaygroundpage/Contents.swift
@@ -21,10 +21,51 @@ class Student: Person {
     var grades: [Grade] = []
 }
 
+class SchoolBandMember: Student {
+    var minimumPracticeTime = 2
+}
+
+class StudentAthlete: Student {
+    override var grades: [Grade] {
+        didSet {
+            if !isEligible {
+                print("It's time to study!")
+            }
+        }
+    }
+
+    var isEligible: Bool {
+        return grades.filter { $0.letter == "F" }.count < 3
+    }
+}
+
 let jon = Person(firstName: "Jon", lastName: "Snon")
 let jane = Student(firstName: "Jane", lastName: "Snane")
+let jessy = SchoolBandMember(firstName: "Jessy", lastName: "Catterwaul")
+let marty = StudentAthlete(firstName: "Marty", lastName: "McWolf")
 jon.firstName
 jane.firstName
+
+let array: [Student] = [jane, jessy, marty]
+
+let student = marty as Student
+let athlete = student as! StudentAthlete
+
+let utterFailureGrade = Grade(letter: "F", points: 0, credits: 0)
+athlete.grades.append(utterFailureGrade)
+athlete.grades.append(utterFailureGrade)
+athlete.grades.append(utterFailureGrade)
+
+func getEveningActivity(student: Student) -> String {
+    if let bandMember = student as? SchoolBandMember {
+        return "Practicing for at least \(bandMember.minimumPracticeTime)"
+    } else {
+        return "Hitting the books"
+    }
+}
+
+getEveningActivity(student: jessy)
+getEveningActivity(student: jane)
 
 let historyGrade = Grade(letter: "B", points: 9, credits: 3)
 jane.grades.append(historyGrade)
