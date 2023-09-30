a3d56782e3b510f8c71d04463480b10a7dd7b2ea
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-30 16:06:11 +0200

    learn inheritance

--- 05_protocols_and_inheritance.playground/Pages/02 - Inheritance.xcplaygroundpage/Contents.swift
+++ 05_protocols_and_inheritance.playground/Pages/02 - Inheritance.xcplaygroundpage/Contents.swift
@@ -2,35 +2,31 @@
 //: ## Episode 02: Inheritance
 
 struct Grade {
-  var letter: Character
-  var points: Double
-  var credits: Double
+    var letter: Character
+    var points: Double
+    var credits: Double
 }
 
 class Person {
-  var firstName: String
-  var lastName: String
-  
-  init(firstName: String, lastName: String) {
-    self.firstName = firstName
-    self.lastName = lastName
-  }
+    var firstName: String
+    var lastName: String
+
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
 }
 
-class Student {
-  var grades: [Grade] = []
-  
-  var firstName: String
-  var lastName: String
-  
-  init(firstName: String, lastName: String) {
-    self.firstName = firstName
-    self.lastName = lastName
-  }
+class Student: Person {
+    var grades: [Grade] = []
 }
 
+let jon = Person(firstName: "Jon", lastName: "Snon")
+let jane = Student(firstName: "Jane", lastName: "Snane")
+jon.firstName
+jane.firstName
 
-
-
+let historyGrade = Grade(letter: "B", points: 9, credits: 3)
+jane.grades.append(historyGrade)
 
 //: [⇒ Next: 03 - Challenge - Inheritance](@next)
