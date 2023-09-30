8261cb26d2e6774d0cff7847849885f487e6b51e
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-30 17:17:25 +0200

    learn initialzers

--- 05_protocols_and_inheritance.playground/Pages/04 - Initializers.xcplaygroundpage/Contents.swift
+++ 05_protocols_and_inheritance.playground/Pages/04 - Initializers.xcplaygroundpage/Contents.swift
@@ -2,27 +2,44 @@
 //: ## Episode 04: Initializers
 
 class Person {
-  var firstName: String
-  var lastName: String
-  
-  init(firstName: String, lastName: String) {
-    self.firstName = firstName
-    self.lastName = lastName
-  }
+    var firstName: String
+    var lastName: String
+
+    required init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
 }
 
 class Student: Person {
-  var grades: [String] = []
+    var grades: [String] = []
+
+    convenience init(transfer: Student) {
+        self.init(firstName: transfer.firstName, lastName: transfer.lastName)
+        grades = transfer.grades
+    }
 }
 
 class StudentAthlete: Student {
-  
+    var sports: [String]
+
+    required convenience init(firstName: String, lastName: String) {
+        self.init(firstName: firstName, lastName: lastName, sports: [])
+    }
+
+    convenience init(transfer: StudentAthlete) {
+        self.init(firstName: transfer.firstName, lastName: transfer.lastName, sports: transfer.sports)
+    }
+
+    init(firstName: String, lastName: String, sports: [String]) {
+        self.sports = sports
+        super.init(firstName: firstName, lastName: lastName)
+    }
 }
 
-StudentAthlete(firstName: "Daniel", lastName: "Ruettiger")
+let rudy = StudentAthlete(firstName: "Daniel", lastName: "Ruettiger", sports: ["Football"])
+StudentAthlete(firstName: "Bernie", lastName: "Kosar")
 
-
-
-
+StudentAthlete(transfer: rudy).sports
 
 //: [⇒ Next: 05 - Challenge: Initializers](@next)
