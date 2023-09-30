06919fbb0f716ab161fade66414961ce1793654c
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-30 13:57:15 +0200

    learn stored properties

--- 04_properties_and_methods.playground/Pages/02 - Stored Properties.xcplaygroundpage/Contents.swift
+++ 04_properties_and_methods.playground/Pages/02 - Stored Properties.xcplaygroundpage/Contents.swift
@@ -1,8 +1,35 @@
 //: [⇐ Previous: 01 - Introduction](@previous)
 //: ## Episode 02: Stored Properties
+struct Wizard {
+    static var commonMagicalIngredients = [
+        "Polyjuice Potion",
+        "Eye of Haystack Needle",
+        "The Force"
+    ] {
+        didSet {
+            print("Magical Ingredients updated! Common stock now contains \(commonMagicalIngredients)")
+        }
+    }
 
+    var firstName: String {
+        willSet {
+            print(firstName + " will be set to " + newValue)
+        }
+        didSet {
+            if firstName.contains(" ") {
+                print("No spaces allowed! \(firstName) is not a first name. Reverting to \(oldValue)")
+                firstName = oldValue
+            }
+        }
+    }
 
+    var lastName: String
+}
 
+var wizard = Wizard(firstName: "Gandalf", lastName: "Greyjoy")
+wizard.firstName = "Hermione"
+wizard.lastName = "Kenobi"
+wizard.firstName = "Merlin Ricewind"
 
-
+Wizard.commonMagicalIngredients.append("Wow-Wow Sauce")
 //: [⇒ Next: 03 - Computed Properties](@next)
