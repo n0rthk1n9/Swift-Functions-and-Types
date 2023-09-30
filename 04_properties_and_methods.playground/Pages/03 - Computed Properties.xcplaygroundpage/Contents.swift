221a1d44e0b08fd9f572bcadffe1d649f79271bb
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-30 14:40:47 +0200

    learn computed properties

--- 04_properties_and_methods.playground/Pages/03 - Computed Properties.xcplaygroundpage/Contents.swift
+++ 04_properties_and_methods.playground/Pages/03 - Computed Properties.xcplaygroundpage/Contents.swift
@@ -1,30 +1,41 @@
 //: [⇐ Previous: 02 - Stored Properties](@previous)
 //: ## Episode 03: Computed Properties
 
-
 struct Wizard {
-  static var commonMagicalIngredients = [
-    "Polyjuice Potion",
-    "Eye of Haystack Needle",
-    "The Force"
-  ] {
-    didSet {
-      print("Magical Ingredients updated! Common stock now contains \(commonMagicalIngredients)")
+    static var commonMagicalIngredients = [
+        "Polyjuice Potion",
+        "Eye of Haystack Needle",
+        "The Force"
+    ] {
+        didSet {
+            print("Magical Ingredients updated! Common stock now contains \(commonMagicalIngredients)")
+        }
     }
-  }
-  
-  var firstName: String {
-    willSet {
-      print(firstName + " will be set to " + newValue)
+
+    var firstName: String {
+        willSet {
+            print(firstName + " will be set to " + newValue)
+        }
+        didSet {
+            if firstName.contains(" ") {
+                print("No spaces allowed! \(firstName) is not a first name. Reverting name to \(oldValue).")
+                firstName = oldValue
+            }
+        }
     }
-    didSet {
-      if firstName.contains(" ") {
-        print("No spaces allowed! \(firstName) is not a first name. Reverting name to \(oldValue).")
-        firstName = oldValue
-      }
+
+    var lastName: String
+
+    var fullName: String {
+        get { return "\(firstName) \(lastName)" }
+        set(newFullName) {
+            let nameSubstrings = newFullName.split(separator: " ")
+            guard nameSubstrings.count >= 2 else {
+                print("\(newFullName) is not a name.")
+                return
+            }
+        }
     }
-  }
-  var lastName: String
 }
 
 var wizard = Wizard(firstName: "Gandalf", lastName: "Greyjoy")
@@ -32,11 +43,5 @@ wizard.firstName = "Hermione"
 wizard.lastName = "Kenobi"
 wizard.firstName = "Merlin Rincewind"
 
-
-
-
-
-
-
-
+wizard.fullName = "SeverusWenderlich"
 //: [⇒ Next: 04 - Lazy Properties](@next)
