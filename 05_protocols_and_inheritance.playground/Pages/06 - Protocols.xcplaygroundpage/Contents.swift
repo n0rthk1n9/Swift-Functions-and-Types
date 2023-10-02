72316660feffc0d02963564a83820a13fc79cca6
Jan Armbrust <jan.armbrust@googlemail.com>
2023-10-02 12:59:59 +0200

    learn protocols

--- 05_protocols_and_inheritance.playground/Pages/06 - Protocols.xcplaygroundpage/Contents.swift
+++ 05_protocols_and_inheritance.playground/Pages/06 - Protocols.xcplaygroundpage/Contents.swift
@@ -1,40 +1,45 @@
 //: [⇐ Previous: 05 - Challenge: Initializers](@previous)
 //: ## Episode 06: Protocols
 
-class Animal {
-    let name: String
+protocol Animal {
+    var name: String { get }
 
-    required init(name: String) {
-        self.name = name
-    }
+    init(name: String)
 
-    func speak() {}
+    func speak()
 }
 
 class Dog: Animal {
+    let name: String
     var tricksLearnedCount: Int
 
     init(name: String, tricksLearnedCount: Int) {
         self.tricksLearnedCount = tricksLearnedCount
-        super.init(name: name)
+        self.name = name
     }
 
     required convenience init(name: String) {
         self.init(name: name, tricksLearnedCount: 0)
     }
 
-    override func speak() {
+    func speak() {
         print("Bow wow! My name is \(name)!")
     }
 }
 
 class Cat: Animal {
-    override func speak() {
+    let name: String
+
+    required init(name: String) {
+        self.name = name
+    }
+
+    func speak() {
         print("My name is \(name). Please leave me alone. I must look at this wall.")
     }
 }
 
-let animals = [Dog(name: "Fang"), Cat(name: "Mr. Midnight")]
+let animals: [Animal] = [Dog(name: "Fang"), Cat(name: "Mr. Midnight")]
 for animal in animals {
     animal.speak()
 }
