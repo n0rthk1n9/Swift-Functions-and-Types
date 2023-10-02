a3a4085f8352d703364ced9d2280e102a934abef
Jan Armbrust <jan.armbrust@googlemail.com>
2023-10-02 12:54:29 +0200

    formatting

--- 05_protocols_and_inheritance.playground/Pages/06 - Protocols.xcplaygroundpage/Contents.swift
+++ 05_protocols_and_inheritance.playground/Pages/06 - Protocols.xcplaygroundpage/Contents.swift
@@ -2,44 +2,41 @@
 //: ## Episode 06: Protocols
 
 class Animal {
-  let name: String
-  
-  required init(name: String) {
-    self.name = name
-  }
-  
-  func speak() { }
+    let name: String
+
+    required init(name: String) {
+        self.name = name
+    }
+
+    func speak() {}
 }
 
 class Dog: Animal {
-  var tricksLearnedCount: Int
-  
-  init(name: String, tricksLearnedCount: Int) {
-    self.tricksLearnedCount = tricksLearnedCount
-    super.init(name: name)
-  }
-  
-  convenience required init(name: String) {
-    self.init(name: name, tricksLearnedCount: 0)
-  }
-  
-  override func speak() {
-    print("Bow wow! My name is \(name)!")
-  }
+    var tricksLearnedCount: Int
+
+    init(name: String, tricksLearnedCount: Int) {
+        self.tricksLearnedCount = tricksLearnedCount
+        super.init(name: name)
+    }
+
+    required convenience init(name: String) {
+        self.init(name: name, tricksLearnedCount: 0)
+    }
+
+    override func speak() {
+        print("Bow wow! My name is \(name)!")
+    }
 }
 
 class Cat: Animal {
-  override func speak() {
-    print("My name is \(name). Please leave me alone. I must look at this wall.")
-  }
+    override func speak() {
+        print("My name is \(name). Please leave me alone. I must look at this wall.")
+    }
 }
 
 let animals = [Dog(name: "Fang"), Cat(name: "Mr. Midnight")]
 for animal in animals {
-  animal.speak()
+    animal.speak()
 }
-
-
-
 
 //: [⇒ Next: 07 - Protocols & Extensions](@next)
